b7be37095b8949618a3213decfeb93e74536b89f
TedCraft <tedwo@yandex.ru>
2021-12-28 11:47:56 +0300

    rework copy

--- commands/music/copy.js
+++ commands/music/copy.js
@@ -16,23 +16,33 @@ module.exports = {
         if (args[0].toLowerCase() == "last" || args[0].toLowerCase() == "l") index = serverQueue.length - 1;
         else if (!isNaN(parseInt(args[0])) && parseInt(args[0]) <= serverQueue.length) index = parseInt(args[0]) - 1;
         else {
-            message.channel.send(`${message.author} Композиция под номером ${index+1} не найдена`);
+            message.channel.send(`${message.author} Композиция под номером ${index + 1} не найдена`);
             return;
         }
 
         let count = 0;
         if (!isNaN(parseInt(args[1]))) count = parseInt(args[1]);
         if (count > 100) count = 100;
-        
-        for (let i = 0; i < count; i++) {
-            insertSong(client, message.guild.id, 
-                serverQueue[index].TITLE.toString('utf8'), 
-                serverQueue[index].URL.toString('utf8'), 
-                serverQueue[index].REQUEST_USER.toString('utf8'), 
-                serverQueue[index].THUMBNAIL_URL.toString('utf8'), 
-                serverQueue[index].LENGTH, 
-                serverQueue[index].CHANNEL_ID.toString('utf8'));
+
+        var elem;
+        let ix = 0;
+        for (const i in serverQueue) {
+            if (ix == index) {
+                elem = serverQueue[i];
+                break;
+            }
+            else {
+                ix++;
+            }
         }
-        message.channel.send(`Песня \`${serverQueue[index].TITLE.toString('utf8')}\` продублирована \`${count} раз\``)
+        for (let i = 0; i < count; i++) {
+            insertSong(client, message.guild.id,
+                elem.TITLE.toString('utf8'),
+                elem.URL.toString('utf8'),
+                elem.REQUEST_USER.toString('utf8'),
+                elem.THUMBNAIL_URL.toString('utf8'),
+                elem.LENGTH);
+        }
+        message.channel.send(`Песня \`${elem.TITLE.toString('utf8')}\` продублирована \`${count} раз\``)
     }
 };
